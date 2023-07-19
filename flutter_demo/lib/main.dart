970d2dd65d4d2e36e1f71d0117ca84709a0f9f48
linyudan <1031378945@qq.com>
2023-07-19 14:06:05 +0800

    实现注册页面

--- flutter_demo/lib/main.dart
+++ flutter_demo/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Button Demo',
+      title: 'TextField Demo',
       home:  MyHomePage(),
     );
   }
@@ -23,47 +25,111 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("我是标题"),
       ),
-      body: TextFieldDemo(),
+      body: contentWidget(),
     );
   }
 }
 
-class TextFieldDemo extends StatefulWidget {
 
+
+
+/// 继承自statefulWidget必须写state，因为在state去维护状态的变化
+/// 整块内容的widget
+class contentWidget extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => _TextFieldDemoState();
+  State<StatefulWidget> createState() {
+    return _contentState();
+  }
+}
+/// 这里用下划线的原因是因为在dart中私有化一个属性是用下划线
+class _contentState extends State<contentWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return loginWidget();
+  }
 }
 
-class _TextFieldDemoState extends State<TextFieldDemo> {
-
-  final textEditController = TextEditingController();
-
+/// 登录的widget
+class loginWidget extends StatefulWidget {
   @override
-  void initState() {
-    super.initState();
-        textEditController.text = "hello world";
-    textEditController.addListener(() {
-      print("textEditController:${textEditController.text}");
-    });
+  State<StatefulWidget> createState() {
+    return _loginState();
+  }
+}
+
+class _loginState extends State<loginWidget> {
+
+  final registerFormkey = GlobalKey<FormState>();
+
+  String username = "";
+  String password = "";
+
+  void saveRegisterForm() {
+
+    if (username.length == 0 || username == "") {
+      print("当前输入的用户名为空$username");
+      return;
+    }
+
+    if (password.length == 0 || password == "") {
+      print("当前输入的密码为空$password");
+      return;
+    }
+
+
+    registerFormkey.currentState?.save();
+    registerFormkey.currentState?.validate();
+    print("当前输入用户名是:$username, 密码是:$password");
+
   }
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        icon: Icon(Icons.people),
-        labelText: "userName",
-        hintText: "请输入用户名",
-        border: InputBorder.none,
-      ),
-      //事件监听
-      onChanged: (value) {
-        print("onChanged：$value");
-      },
-      //键盘的提交按钮
-      onSubmitted: (value) {
-        print("onSubmitted:$value");
-      },
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Form(
+          child: Column( //child:这里只能存放一个textField
+            key: registerFormkey,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [ //children:这里就可以存放多个textField
+
+              TextFormField( //用户名的textField
+                decoration: const InputDecoration( //这里存放着textField中的icon
+                  icon: Icon(Icons.people),
+                  hintText: "请输入用户名或手机号码",
+                ),
+                onChanged: (value) {
+                  this.username = value ?? "";
+                  print("$value");
+                },
+              ),
+
+              SizedBox(height: 20,),
+
+              TextFormField( //密码的TextField
+                obscureText: true,
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.lock),
+                  hintText: "请输入密码",
+                  suffixIcon: ImageIcon(image)
+                ),
+                onChanged: (value) {
+                  this.password = value ?? "";
+                  print("$value");
+                },
+              ),
+
+              SizedBox(height: 20,),
+
+              Container(
+                width: double.infinity,
+                height: 44,
+                child: ElevatedButton(onPressed: saveRegisterForm,
+                  child: Text("注册",style: TextStyle(fontSize: 20,color: Colors.white),),
+                ),
+              ),
+            ],
+      )),
     );
   }
 }
